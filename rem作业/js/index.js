e9be07c13d46d1a1b25d65e67f03883cd2e8b17e
764706069 <764706069@qq.com>
2018-05-06 22:12:20 +0800

    add

---
+++ rem作业/js/index.js
@@ -0,0 +1,15 @@
+var fun = function (doc, win) {
+	var docEl = doc.documentElement,
+	    resizeEvt = 'orientionchange' in window ? 'orientionchange' : 'resize',
+	    recalc = function () {
+	    	var clientWidth = docEl.clientWidth;
+	    	if (!clientWidth)
+	    		return;
+	    	docEl.style.fontSize = 20 * (clientWidth / 640) + 'px';
+	    };
+	if (!doc.addEventListener)
+		return;
+	win.addEventListener(resizeEvt, recalc, false);
+	doc.addEventListener('DOMContentLoaded', recalc, false);
+}
+fun(document, window);
